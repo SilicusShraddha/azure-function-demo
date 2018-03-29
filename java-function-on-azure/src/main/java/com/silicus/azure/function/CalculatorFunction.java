5dba97853b5055333d94dba52aafe9daecf68c5a
SilicusShraddha <shraddha.hambir@silicus.com>
2018-03-29 15:01:03 +0530

    First Commit
    Uploading all files

---
+++ java-function-on-azure/src/main/java/com/silicus/azure/function/CalculatorFunction.java
@@ -0,0 +1,37 @@
+package com.silicus.azure.function;
+
+import java.util.Optional;
+
+import com.microsoft.azure.serverless.functions.ExecutionContext;
+import com.microsoft.azure.serverless.functions.HttpRequestMessage;
+import com.microsoft.azure.serverless.functions.HttpResponseMessage;
+import com.microsoft.azure.serverless.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.serverless.functions.annotation.FunctionName;
+import com.microsoft.azure.serverless.functions.annotation.HttpTrigger;
+import com.silicus.azure.Calculator;
+
+public class CalculatorFunction 
+{
+   
+        @FunctionName("calulatorFunction")
+        public HttpResponseMessage<String> calulatorFunction(@HttpTrigger(name = "req", methods = {"get", "post"}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+                final ExecutionContext context) {
+            context.getLogger().info("Java HTTP trigger processed a request.");
+
+            // Parse query parameter
+            String query = request.getQueryParameters().get("name");
+            
+            String query1 = request.getQueryParameters().get("name1");
+            String name = request.getBody().orElse(query);
+            
+
+            if (name == null) {
+                return request.createResponse(400, "Please pass a name on the query string or in the request body");
+            } else {
+            	Calculator cal = new Calculator();
+                return request.createResponse(200, query1+"***"+ cal.echoWord(name));
+            }
+        }
+  
+}
+   
